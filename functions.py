185350a8ee867621e8824aa52aebff458798a7e0
zudzuka <zudzuka@gmail.com>
2015-09-23 14:58:59 +0300

    Добавил кнопку скачать

--- functions.py
+++ functions.py
@@ -10,15 +10,16 @@ import PyQt4
 sys.path.append('..')
 from user import login, password, token
 
-webbrowser.open('https://oauth.vk.com/authorize?client_id=5073025&display=page&redirect_uri=https://oauth.vk.com/blank.html&response_type=token&v=5.37')
+#webbrowser.open('https://oauth.vk.com/authorize?client_id=5073025&display=page&redirect_uri=https://oauth.vk.com/blank.html&response_type=token&v=5.37')
 
-vkapi = vk.API('5073025', login, password, access_token=token)
-
-class Person(object):
+class Person():
     elo = 0                     # Elo rating
     group = 0                   # Matchmaking group
     k = 40                      # Initial coefficient
     battles = 0                 # Batlles was played
+
+
+vkapi = vk.API('5073025', login, password, access_token=token)
 
 def unpickle(): 
     data = open('data.pkl', 'rb')
@@ -31,28 +32,28 @@ def save(self):
     pickle.dump(self, data)
     data.close()
 
-def download(self): # Download library
+def download(): # Download library
     profiles = vkapi.users.search(
-        sort=1, 
+        sort=0, 
         #sex=1, 
         has_photo=1, 
         #university=1,
         #university_year=2016,
         #university_faculty=1,
-        count=100,
+        count=10,
         fields='photo_max_orig'
         )
 
     print(len(profiles['items']))
 
+    array = []
     # Parsing library to array of Person() class
     for i in range(len(profiles['items'])):
-        self.append(Person())
-        self[i].name    = profiles['items'][i]['first_name']
-        self[i].sname = profiles['items'][i]['last_name']
-        self[i].id  = profiles['items'][i]['id']
-        self[i].jpg = r'pics/'+str(profiles['items'][i]['id'])+'.jpg'
-
+        array.append(Person())
+        array[i].name    = profiles['items'][i]['first_name']
+        array[i].sname = profiles['items'][i]['last_name']
+        array[i].id  = profiles['items'][i]['id']
+        array[i].jpg = r'pics/'+str(profiles['items'][i]['id'])+'.jpg'
 
     # Download images 
     for i in range(len(profiles['items'])):
@@ -62,7 +63,14 @@ def download(self): # Download library
         out.write(p.content)
         out.close()
 
-peoples=[]
-download(peoples)
-save(peoples)
+    save(array)
 
+def buttle(w, l):
+    we=w.elo
+    le=l.elo
+    w.elo=we+w.k*(1-1/(1+pow(10, (le-we)/400)))
+    l.elo=we+w.k*(1-1/(0+pow(10, (le-we)/400)))
+    w.battles=w.battles+1
+    l.battles=l.battles+1
+    if w.battles>30: w.k=20
+    if l.battles>30: l.k=20
